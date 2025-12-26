45a5ec91f88d23f3864ec823d24e84653e1988d4
someshwar-kumbar <tncxsomu@gmail.com>
2025-12-27 02:53:24 +0530

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -7,4 +7,4 @@ def test_products_details():
         "Quantity:2\n"
         "Price:70000\n"
     )
-assert product_details(107, "iphone", 2, 70000) == expected_output
+    assert product_details(107, "iphone", 2, 70000) == expected_output
